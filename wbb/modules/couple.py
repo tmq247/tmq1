69973f9fed4e2632b0888091d756d3af07d4ef9f
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-07-19 20:27:59 +0700

    Update couple.py

--- wbb/modules/couple.py
+++ wbb/modules/couple.py
@@ -32,7 +32,7 @@ from wbb.core.decorators.errors import capture_err
 from wbb.utils.dbfunctions import get_couple, save_couple
 
 __MODULE__ = "Shippering"
-__HELP__ = "/detect_gay - To Choose Couple Of The Day"
+__HELP__ = "/detect_gay - Để Chọn Cặp Đôi Trong Ngày"
 
 
 # Date and time
@@ -71,9 +71,9 @@ def tomorrow():
 @capture_err
 async def couple(_, message):
     if message.chat.type == enums.ChatType.PRIVATE:
-        return await message.reply_text("This command only works in groups.")
+        return await message.reply_text("Lệnh này chỉ hoạt động trong nhóm.")
 
-    m = await message.reply("Detecting gay among us...")
+    m = await message.reply("Phát hiện đồng tính giữa chúng ta...")
 
     try:
         chat_id = message.chat.id
@@ -85,7 +85,7 @@ async def couple(_, message):
                     user = await app.get_users(i.user.id)
                     list_of_users.append(user.id)
             if len(list_of_users) < 2:
-                return await m.edit("Not enough users")
+                return await m.edit("Không đủ người dùng")
             c1_id = random.choice(list_of_users)
             c2_id = random.choice(list_of_users)
             while c1_id == c2_id:
@@ -93,10 +93,10 @@ async def couple(_, message):
             c1_mention = (await app.get_users(c1_id)).mention
             c2_mention = (await app.get_users(c2_id)).mention
 
-            couple_selection_message = f"""**Couple of the day:**
+            couple_selection_message = f"""**Cặp đôi của ngày:**
 {c1_mention} + {c2_mention} = ❤️
 
-__New couple of the day may be chosen at 12AM {tomorrow()}__"""
+__Cặp đôi mới trong ngày có thể được chọn lúc 12 giờ trưa {tomorrow()}__"""
             await m.edit(couple_selection_message)
             couple = {"c1_id": c1_id, "c2_id": c2_id}
             await save_couple(chat_id, today(), couple)
@@ -109,7 +109,7 @@ __New couple of the day may be chosen at 12AM {tomorrow()}__"""
             couple_selection_message = f"""Couple of the day:
 [{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = ❤️
 
-__New couple of the day may be chosen at 12AM {tomorrow()}__"""
+__Cặp đôi mới trong ngày có thể được chọn lúc 12 giờ trưa {tomorrow()}__"""
             await m.edit(couple_selection_message)
     except Exception as e:
         print(e)
