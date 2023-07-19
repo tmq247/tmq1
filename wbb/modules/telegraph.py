2c04759f506048d6212f54b5cbe452d15e48c12a
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-07-19 21:55:54 +0700

    Update telegraph.py

--- wbb/modules/telegraph.py
+++ wbb/modules/telegraph.py
@@ -5,7 +5,7 @@ from wbb import app, telegraph
 from wbb.core.decorators.errors import capture_err
 
 __MODULE__ = "Telegraph"
-__HELP__ = "/telegraph [Page name]: Paste styled text on telegraph."
+__HELP__ = "/telegraph [Page name]: Dán văn bản theo kiểu trên điện báo."
 
 
 @app.on_message(filters.command("telegraph"))
@@ -14,16 +14,16 @@ async def paste(_, message: Message):
     reply = message.reply_to_message
 
     if not reply or not reply.text:
-        return await message.reply("Reply to a text message")
+        return await message.reply("Trả lời tin nhắn văn bản")
 
     if len(message.command) < 2:
-        return await message.reply("**Usage:**\n /telegraph [Page name]")
+        return await message.reply("**Cách sử dụng:**\n /telegraph [Page name]")
 
     page_name = message.text.split(None, 1)[1]
     page = telegraph.create_page(
         page_name, html_content=reply.text.html.replace("\n", "<br>")
     )
     return await message.reply(
-        f"**Posted:** {page['url']}",
+        f"**Đã đăng:** {page['url']}",
         disable_web_page_preview=True,
     )
