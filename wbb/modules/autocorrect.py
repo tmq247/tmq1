d42fb123b562db341faf66b29c91d405fe678dc2
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-07-19 20:11:31 +0700

    Update autocorrect.py

--- wbb/modules/autocorrect.py
+++ wbb/modules/autocorrect.py
@@ -9,14 +9,14 @@ from wbb.utils.filter_groups import autocorrect_group
 @app.on_message(filters.command("autocorrect"))
 async def autocorrect_bot(_, message: Message):
     if not message.reply_to_message:
-        return await message.reply_text("Reply to a text message.")
+        return await message.reply_text("Trả lời tin nhắn văn bản.")
     reply = message.reply_to_message
     text = reply.text or reply.caption
     if not text:
-        return await message.reply_text("Reply to a text message.")
+        return await message.reply_text("Trả lời tin nhắn văn bản.")
     data = await arq.spellcheck(text)
     if not data.ok:
-        return await message.reply_text("Something wrong happened.")
+        return await message.reply_text("Đã xảy ra sự cố.")
     result = data.result
     await message.reply_text(result.corrected if result.corrected else "Empty")
 
@@ -33,16 +33,16 @@ IS_ENABLED = False
 async def autocorrect_ubot_toggle(_, message: Message):
     global IS_ENABLED
     if len(message.command) != 2:
-        return await eor(message, text="Not enough arguments.")
+        return await eor(message, text="Không đủ đối số.")
     state = message.text.split(None, 1)[1].strip().lower()
     if state == "enable":
         IS_ENABLED = True
-        await eor(message, text="Enabled!")
+        await eor(message, text="Đã bật!")
     elif state == "disable":
         IS_ENABLED = False
-        await eor(message, text="Disabled!")
+        await eor(message, text="Đã tắt!")
     else:
-        return await eor(message, text="Wrong argument, Pass (ENABLE|DISABLE).")
+        return await eor(message, text="Đối số sai, đạt (ENABLE|DISABLE).")
 
 
 @app2.on_message(
