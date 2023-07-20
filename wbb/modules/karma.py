d8bffabf8e942b7b8aad6c4995a36ba3d18b5f76
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-07-20 12:29:08 +0700

    Update karma.py

--- wbb/modules/karma.py
+++ wbb/modules/karma.py
@@ -87,7 +87,7 @@ async def upvote(_, message):
         new_karma = {"karma": karma}
         await update_karma(chat_id, await int_to_alpha(user_id), new_karma)
     await message.reply_text(
-        f"Tăng Karma của {user_mention} thêm 1 \nTổng số điểm: {karma}"
+        f"Tăng nghiệp của {user_mention} thêm 1 \nTổng số điểm: {karma}"
     )
 
 
@@ -138,7 +138,7 @@ async def downvote(_, message):
         new_karma = {"karma": karma}
         await update_karma(chat_id, await int_to_alpha(user_id), new_karma)
     await message.reply_text(
-        f"Giảm Karma của {user_mention} đi 1 \nTổng số điểm: {karma}"
+        f"Giảm nghiệp của {user_mention} đi 1 \nTổng số điểm: {karma}"
     )
 
 
