3cbc74ed5ac44bc4a63aa25ebd51b6b80c2b4682
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-07-20 13:13:14 +0700

    Update repo.py

--- wbb/modules/repo.py
+++ wbb/modules/repo.py
@@ -28,14 +28,14 @@ from wbb.core.decorators.errors import capture_err
 from wbb.utils.http import get
 
 __MODULE__ = "Repo"
-__HELP__ = "/repo - To Get My Github Repository Link " "And Support Group Link"
+__HELP__ = "/repo -Để Nhận Liên kết Kho lưu trữ Github của tôi " "Và Liên kết Nhóm Hỗ trợ"
 
 
 @app.on_message(filters.command("repo"))
 @capture_err
 async def repo(_, message):
     users = await get(
-        "https://api.github.com/repos/thehamkercat/williambutcherbot/contributors"
+        "@coihaycoc"
     )
     list_of_users = ""
     count = 1
@@ -43,9 +43,9 @@ async def repo(_, message):
         list_of_users += f"**{count}.** [{user['login']}]({user['html_url']})\n"
         count += 1
 
-    text = f"""[Github](https://github.com/thehamkercat/WilliamButcherBot) | [Group](t.me/PatheticProgrammers)
+    text = f"""[Github](@coihaycoc) | [Group](@coihaycoc)
 ```----------------
-| Contributors |
+| người đóng góp |
 ----------------```
 {list_of_users}"""
     await app.send_message(message.chat.id, text=text, disable_web_page_preview=True)
